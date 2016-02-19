cd2958b7b48c9db1b1efbf7ce8c2431fbb3feeb0
Ayrton-Neacail <ayrtona90@gmail.com>
2016-02-19 14:26:46 +0200

    for loop and if statements coded

---
+++ Tutorial one- methods3/SourceTutOne.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int x;
+
+	for (x = 1; x < 21; x++){
+		cout << x << endl;
+
+		if (x % 3 == 0){
+			cout << "Fizz" << endl;
+		}
+		else if (x%5==0){
+			cout << "Buzz" << endl;
+		}
+		else if (x % 3 == 0 && x % 5 == 0){
+			cout << "FizzBuzz" << endl;
+		}
+		else{
+			cout << x << endl;
+		}
+	}
+
+
+}
