5ee48b2cb4b1865481750079cb18dab8e6d4c064
Ayrton-Neacail <ayrtona90@gmail.com>
2016-02-19 14:50:43 +0200

    1-50 branch coding created

--- Tutorial one- methods3/SourceTutOne.cpp
+++ Tutorial one- methods3/SourceTutOne.cpp
@@ -6,7 +6,7 @@ int main()
 
 	int x;
 
-	for (x = 0; x < 21; x++){
+	for (x = 0; x < 51; x++){
 		cout << x << endl;
 
 		if (x % 3 == 0){
@@ -15,8 +15,17 @@ int main()
 		else if (x%5==0){
 			cout << "Buzz" << endl;
 		}
+		else if (x % 7 == 0){
+			cout << "woof" << endl;
+		}
 		else if (x % 3 == 0 && x % 5 == 0){
 			cout << "FizzBuzz" << endl;
+		}
+		else if (x%3==0 && x%7==0){
+			cout << "FizzWoof" << endl;
+		}
+		else if (x % 5 == 0 && x % 7 == 0){
+			cout << "BuzzWoof" << endl;
 		}
 		else{
 			cout << x << endl;
